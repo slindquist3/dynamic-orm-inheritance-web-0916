9808494c9c9e596b3218aecd6980a18bccc13c52
slindquist3 <slindquist3@gmail.com>
2016-10-09 18:59:49 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,7 +32,7 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def table_name_for_insert
+  def table_name_for_insert 
     self.class.table_name
   end
 
